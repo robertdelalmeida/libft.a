d30e9d2cc5386671ae9e6e9928a9a2e53b28a2ef
Roberto Del Franco <robertinhodf@gmail.com>
2024-10-16 17:34:29 -0300

    fixing substr

--- ft_substr.c
+++ ft_substr.c
@@ -19,8 +19,10 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 
 	if (!s)
 		return (NULL);
-	if (start > ft_strlen(s) || start + len > ft_strlen(s))
+	if (start > ft_strlen(s))
 		return (ft_strdup(""));
+	if (len > start + ft_strlen(s))
+		len = ft_strlen(s) - start;
 	sub = (char *)malloc((len + 1) * sizeof(char));
 	if (!sub)
 		return (NULL);
